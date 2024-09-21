1a4e47ed71684831aa0058a8697a8709cc474918
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-09-21 12:31:05 +0900

    TodoListのDELETEを実装

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo:(id:string)=>void;
 }
 
-const TodoList: React.FC<TodoListProps> = props => {
+const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todos) => (
-        <li key={todos.id}>{todos.text}</li>
+        <li key={todos.id}>
+          <span>{todos.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null,todos.id)}>DELETE</button>
+        </li>
       ))}
     </ul>
   );
